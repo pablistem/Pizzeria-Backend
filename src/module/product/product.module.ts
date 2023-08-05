60c948c20b5b97657babe02e4d189408e9738509
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-08-04 22:10:54 -0300

    fix: :bug: dependencies ProductModule

--- src/module/product/product.module.ts
+++ src/module/product/product.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './application/service/product.service';
 import { ProductController } from './interface/product.controller';
+import { ProductRepository } from './infrastructure/product.repository';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, ProductRepository],
 })
 export class ProductModule {}
