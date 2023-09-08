e001bdb0bfb82b7d969b21d430ff794ece0a5f06
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-09-07 21:46:12 -0300

    fix: :bug: enable valitarionpipes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 declare const module: any;
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-
+  
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
   const config = new DocumentBuilder()
