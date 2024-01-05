e47933b2ee72b98fb430daffe9d3e0c4c4859cea
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-05 02:19:24 -0300

    Add fixtures tree

---
+++ src/common/fixtures/fixtureTree.ts
@@ -0,0 +1,15 @@
+import { category1, category2, category3 } from './category';
+import { item1, item2, item3 } from './item';
+import { option1, option2 } from './option';
+import { order1, order2 } from './order';
+import { product1, product2, product3 } from './product';
+import { adminUser, anonUser, normalUser } from './user';
+
+export const fixturesTree = {
+  User: [normalUser, adminUser, anonUser],
+  Category: [category1, category2, category3],
+  Product: [product1, product2, product3],
+  Option: [option1, option2],
+  Order: [order1, order2],
+  Item: [item1, item2, item3],
+};
