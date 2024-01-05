8b419148cdfce912379e3a9cfe0174c98c64a49e
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-05 02:18:26 -0300

    Update product.ts for fixture loading

--- src/common/fixtures/product.ts
+++ src/common/fixtures/product.ts
@@ -1,44 +1,33 @@
-import { Product } from 'src/module/product/domain/product.entity';
 import { category1, category2, category3 } from './category';
-import { option1, option2 } from './option';
 
-export const product1: Product = {
+export const product1 = {
   id: 1,
   title: 'product_1',
   description: 'product_1',
   image: 'img_1',
-  category: category1,
+  category: category1.id,
   price: 200,
   stock: 20,
-  options: [option1, option2],
-  createdAt: undefined,
-  updatedAt: undefined,
 };
 
-export const product2: Product = {
+export const product2 = {
   id: 2,
   title: 'product_2',
   description: 'product_2',
   image: 'image',
-  category: category2,
+  category: category2.id,
   price: 10,
   stock: 10,
-  options: undefined,
-  createdAt: undefined,
-  updatedAt: undefined,
 };
 
-export const product3: Product = {
+export const product3 = {
   id: 3,
   title: 'product_3',
   description: 'product_3',
   image: 'image',
-  category: category3,
+  category: category3.id,
   price: 10,
   stock: 10,
-  options: undefined,
-  createdAt: undefined,
-  updatedAt: undefined,
 };
 
 export const productFixtures = [product1, product2, product3];
