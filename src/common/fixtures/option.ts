d3f08fae4bb875d93fa2831d2c6da825b843b4d8
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-05 02:17:41 -0300

    Update option.ts for fixture loading

--- src/common/fixtures/option.ts
+++ src/common/fixtures/option.ts
@@ -1,21 +1,17 @@
-import { Option } from '../../module/option/domain/option.entity';
+import { product1, product3 } from './product';
 
-export const option1: Option = {
+export const option1 = {
   id: 1,
   variant: 'option_1',
   price: 10,
-  product: undefined,
-  createdAt: undefined,
-  updatedAt: undefined,
+  product: product3.id,
 };
 
-export const option2: Option = {
+export const option2 = {
   id: 2,
   variant: 'option_2',
   price: 10,
-  product: undefined,
-  createdAt: undefined,
-  updatedAt: undefined,
+  product: product1.id,
 };
 
 export const optionFixtures = [option1, option2];
