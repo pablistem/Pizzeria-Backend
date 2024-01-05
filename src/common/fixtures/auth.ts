21eeddcf17b3e8d561a5a541564283e23b902719
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-05 02:13:35 -0300

    Update auth for fixture loading

--- src/common/fixtures/auth.ts
+++ src/common/fixtures/auth.ts
@@ -1,23 +1,18 @@
-import { Auth } from 'src/module/auth/domain/auth.entity';
 import { adminUser, normalUser } from './user';
 
 export const refreshTokenAdmin =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMiLCJlbWFpbCI6ImFkbWluQGVtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiJ9.0b9BbjpBFg7eEES4jOk6kjjNCYQLWLfM_4RMxNz382k';
 export const refreshTokenUser =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIiLCJlbWFpbCI6Im5vcm1hbEBlbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9.L60KAcjoA1a6qTNXF2nAdgO9sUNLPmzrpbUPemRTvIc';
-export const sessionAdmin: Auth = {
+export const sessionAdmin = {
   id: 1,
-  createdAt: undefined,
-  updatedAt: undefined,
-  user: adminUser,
+  user: adminUser.id,
   refreshToken: refreshTokenAdmin,
 };
 
-export const sessionNormal: Auth = {
+export const sessionNormal = {
   id: 2,
-  createdAt: undefined,
-  updatedAt: undefined,
-  user: normalUser,
+  user: normalUser.id,
   refreshToken: refreshTokenUser,
 };
 
