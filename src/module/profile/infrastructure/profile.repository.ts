d6dd5848b60ba74c4c3d62ed1fd6ffeb0da7bbaf
Facundo Villafañe <facundoagustinvillafane@gmail.com>
2024-01-17 18:22:53 -0300

    Fixed Test Profile

--- src/module/profile/infrastructure/profile.repository.ts
+++ src/module/profile/infrastructure/profile.repository.ts
@@ -1,36 +1,35 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
-import { IProfileRepository } from "../application/repository/profile.repository.interface";
-import { Profile } from "../domain/profile.entity";
-import { ICreateProfile, IUpdateProfile } from "../interface/profile.entity";
+import { IProfileRepository } from '../application/repository/profile.repository.interface';
+import { Profile } from '../domain/profile.entity';
+import { ICreateProfile, IUpdateProfile } from '../interface/profile.entity';
 
-@Injectable() 
+@Injectable()
 export class ProfileRepository implements IProfileRepository {
-    repository: Repository<Profile>
+  repository: Repository<Profile>;
 
-    constructor(private readonly datasource: DataSource) {
-        this.repository = datasource.getRepository(Profile)
-    }
+  constructor(private readonly datasource: DataSource) {
+    this.repository = datasource.getRepository(Profile);
+  }
 
-    async findAll(): Promise<Profile[]> {
-        return this.repository.find();
-    };
+  async findAll(): Promise<Profile[]> {
+    return this.repository.find();
+  }
 
-    async findOne(id: number): Promise<Profile | null> {
-        return await this.repository.findOne({
-            where: {
-                id: id,
-            },
-            relations: ['user']
-        });
-    };
-    
-    async createProfile(newProfile: ICreateProfile): Promise<void> {
-        const profileNew = this.repository.create(newProfile);
-        this.repository.save(profileNew);
-    };
+  async findOne(id: number): Promise<Profile | null> {
+    return await this.repository.findOne({
+      where: {
+        id: id,
+      },
+    });
+  }
 
-    async updateProfile(changes: IUpdateProfile): Promise<Profile | null> {
-        return this.repository.save(changes);
-    };
+  async createProfile(newProfile: ICreateProfile): Promise<void> {
+    const profileNew = this.repository.create(newProfile);
+    this.repository.save(profileNew);
+  }
+
+  async updateProfile(changes: IUpdateProfile): Promise<Profile | null> {
+    return this.repository.save(changes);
+  }
 }
